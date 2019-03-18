73cc53f43b25d5e7d7f51eac7d4149984d38a8d6
Nuno Nogueira <nunonogueira@MBP-de-Nuno.home>
2019-03-18 21:47:43 +0000

    h

---
+++ jogos.c
@@ -0,0 +1,53 @@
+/*****************************************************************/
+/*         Trabalho pratico 1 | PROG2 | MIEEC | 2018/19          */
+/*****************************************************************/
+/*                    FUNCOES A IMPLEMENTAR                      */
+/*****************************************************************/
+
+#include "jogos.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+vetor* jogos_load(const char *nomef){
+//fsdf
+    return NULL;
+}
+
+
+int jogos_save(vetor *vec, const char *nomef){
+//fsdf
+    return -1;
+}
+
+
+vetor_equipas *stats_equipa(vetor *vec){
+//fsdf
+    return NULL;
+}
+
+
+int equipas_ordena(vetor_equipas *v, int criterio){
+//fsdf
+    return -1;
+}
+
+
+int corrige_jogo(vetor *vec, const char *nomef){
+//fsdf
+    return -1;
+}
+
+
+int pontos_de_equipa(vetor *vec, char *nome_equipa, char *epoca){
+    //fsdf
+    return -1;
+}
+
+
+
+
+
+
+
