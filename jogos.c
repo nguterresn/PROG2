513b90398bd20d60973c2f475adcfa5d6bd6381c
Nuno Nogueira <nunonogueira@MBP-de-Nuno.home>
2019-03-19 18:23:07 +0000

    checking spaces between strings

--- jogos.c
+++ jogos.c
@@ -31,7 +31,8 @@ vetor* jogos_load(const char *nomef){
             jogo * aux = vetor_elemento(vtr, i);
 			printf ("Pos %d -> %s", i, aux);
 		}*/
-                
+
+        free(jogo1);     
         fclose(file);
         return vtr;
     } 
@@ -41,9 +42,15 @@ vetor* jogos_load(const char *nomef){
 
 int jogos_save(vetor *vec, const char *nomef){
 
+    int len;
+    char convert;
     FILE *file;
     file = fopen(nomef,"w");
-    char buffer[255];
+
+    jogo * jg = (jogo*)malloc(sizeof(jogo));
+    memset(jg->epoca, 0, sizeof(jg->epoca));
+    memset(jg->nome_casa, 0, sizeof(jg->nome_casa));
+    char * buffer = (char*) malloc (255 * sizeof(*buffer));
 
     /* nao usado, necessario alocar */
     if (vec == NULL) 
@@ -53,21 +60,71 @@ int jogos_save(vetor *vec, const char *nomef){
             return -1;
     } 
 
-    jogo * jogo1 = (jogo*)malloc(sizeof(jogo));
-
     for (int i = 0; i < vec->tamanho; i++) 
     {
-        jogo * aux = vetor_elemento(vec, i);
-        fprintf((FILE*) file, "%s", aux);
+        jogo * jog = vetor_elemento(vec, i);
+        fprintf((FILE*) file, "%s", jog);
     }
 
+    for(int i = 0; i < 10; i++)
+    {
+        strcpy(buffer, (const char*)vetor_elemento(vec, i));
+        /* Mais tarde, substituir o buffer por jg com memória, não esta a dar neste momento 
+        for (int j = 0; j < strlen (buffer); j++) {
+            printf("%c", buffer[j]);
+        }*/
+
+        len = 0;
+        memcpy(jg->epoca, buffer, 5);
+
+        printf ("\n\n\nEpoca: ");
+        for (int j = 0; j < 10; j++) {
+            printf("%c", jg->epoca[j]);
+        }
+
+        while (buffer[len] != ' ') len++;
+
+        printf ("\nLen: %d", len);
+
+        while (buffer[len+1] != ' ') len++;
+        
+        printf ("\nLen2: %d", len);
+
+        while (buffer[len] != ' ') len++;
+        
+        printf ("\nLen3: %d", len);
+
+        while (buffer[len+1] != ' ') len++;
+        
+        printf ("\nLen4: %d", len);
+
+        while (buffer[len] != ' ') len++;
+        
+        printf ("\nLen5: %d", len);
+
+        while (buffer[len+1] != ' ') len++;
+        
+        printf ("\nLen6: %d", len);
+
+    }
+    
     fclose(file);
     return vec->tamanho;
 }
 
 
 vetor_equipas *stats_equipa(vetor *vec){
-//fsdf
+
+    vetor_equipas * vtr_equipas = vetor_equipas_novo();
+    if (vtr_equipas == NULL)
+        return NULL;
+    
+    /*for (int i = 0; i < vec->tamanho; i++) 
+    {
+        jogo * aux = vetor_elemento(vec, i);
+    }*/
+    
+    // ultimo passo: vetor_equipas_insere()...
     return NULL;
 }
 
